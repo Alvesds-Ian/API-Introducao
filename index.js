96d8d5d9e670731c1d5a965e9139c224266305a6
Ian Alves <132668846+Alvesds-Ian@users.noreply.github.com>
2023-08-11 11:02:58 +0000

    adicionado post

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-import { selectUsuarios, selectUsuario } from "./bd.js";
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
 
 app.get("/usuarios", async (req, res) => {
   console.log("Rota GET/usuarios solicitada");
@@ -35,6 +35,18 @@ app.get("/usuario/:id", async (req, res) => {
     const usuario = await selectUsuario(req.params.id);
     if (usuario.length > 0) res.json(usuario);
     else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.use(express.json());
+
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
